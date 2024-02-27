5cf676f7e3b6be763f07f72d4ba148ee9f1a19a3
Anna Soboleva <asoboleva.avs@gmail.com>
2024-02-28 01:25:21 +0300

    added @pytest.fixture

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,15 +1,31 @@
 from utils import dicts
+import pytest
 
 
-def test_get_val():
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 1, "test") == 'one'
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 5, "test1") == "test1"
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, -5, "test1") == "test1"
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 'tt', "test1") == "test1"
-    assert dicts.get_val({1: 'one', 2: 'two', 3: 'three'}, 0) == "git"
-    assert dicts.get_val({1: 'one'}, 0) == "git"
-    assert dicts.get_val({1: 'one'}, 1) == "one"
-    assert dicts.get_val({1: 'one'}, 5, 'test2') == "test2"
-    assert dicts.get_val({}, 0) == "git"
-    assert dicts.get_val({}, 'test3') == "git"
-    assert dicts.get_val({}, 5, 'test3') == "test3"
+@pytest.fixture
+def dict1():
+    return {1: 'one', 2: 'two', 3: 'three'}
+
+
+@pytest.fixture
+def dict2():
+    return {'1': 'one'}
+
+
+@pytest.fixture
+def dict3():
+    return {}
+
+
+def test_get_val(dict1, dict2, dict3):
+    assert dicts.get_val(collection=dict1, key=1, default="test") == 'one'
+    assert dicts.get_val(dict1, 5, "test1") == "test1"
+    assert dicts.get_val(dict1, -5, "test1") == "test1"
+    assert dicts.get_val(dict1, 'tt', "test1") == "test1"
+    assert dicts.get_val(dict1, 0) == "git"
+    assert dicts.get_val(dict2, 0) == "git"
+    assert dicts.get_val(dict2, '1') == "one"
+    assert dicts.get_val(dict2, 5, 'test2') == "test2"
+    assert dicts.get_val(dict3, 0) == "git"
+    assert dicts.get_val(dict3, 'test3') == "git"
+    assert dicts.get_val(dict3, 5, 'test3') == "test3"
